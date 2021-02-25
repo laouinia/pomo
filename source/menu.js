d2532a52beb4f3db2cee2758334e0557126f792e
la <laouinia@yahoo.com>
2021-02-24 18:23:02 -0800

    test

---
+++ source/menu.js
@@ -0,0 +1,10 @@
+
+/* Menu js */
+
+function openNav() {
+  document.getElementById("sideTask").style.width = "350px";
+}
+
+function closeNav() {
+  document.getElementById("sideTask").style.width = "0";
+}
